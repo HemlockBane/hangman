ba13912a404196015aa94eeedb551e35f7adc11e
Obinna Igwe <igwenus619@gmail.com>
2020-09-25 08:52:53 +0100

    Reduce user's life if user's guess is incorect; User wins immiediately all the letters are guessed correctly

--- hangman.py
+++ hangman.py
@@ -1,50 +1,87 @@
-import random as r
+# Hangman Game
 
-# ....
-# User has 8 tries to the guess the letters in the word
-# The user loses a try after every guess
-# Once the user makes a correct guess, uncover all the matching letters in the word
-# Once the user makes a wrong guess, display an error message
-# Once the user has used up all his tries, show a message
+
+# Keep on looping if user still has lives (total lives count is 8)
+# During the loop:
+# - If user meets win condition #1, react accordingly
+# - if user meets a life loss condition, react accordingly
+# At the end of the loop, check for fail condition #1 & win condition #2
+
+
+
+# Life loss conditions:
+# 1. If a letter doesn't occur in the word:
+# - Print "That letter does'nt appear in the word"
+# - Reduce the number of remaining attempts
+
+# 2. If a letter occurs in the word but has been guessed by the user,
+# - print "No improvements"
+# - Reduce the number of remaining attempts
+
+
+# Fail conditions
+# 1. remaining attempts is 0 and user has not guessed all the words
+# - print "You lost!"
+
+
+# Win conditions
+# 1. If remaining attempts is greater than or equal to 1 (i.e. during the loop)
+# after printing masked text, if all the letters have been guessed
+# - print "You guessed the word!"
+# - print "You survived!"
+# break out of loop
+
+# 2. If remaining attempts is 0 and all the letters have been guessed
+# - print "You guessed the word!"
+# - print "You survived!"
+
+
+import random as r
 
 list_of_options = ['python', 'java', 'kotlin', 'javascript']
 
-# Choose a random word from a list of options
 selected_word = r.choice(list_of_options)
-# blank_spaces = (len(correct_word) - 3) * '_'
-# hint = f"{correct_word[:3]}{blank_spaces}"
 
 masked_text = len(selected_word) * '-'
 
+
+# print(selected_word)
+
 print("H A N G M A N")
 print("")
-for i in range(8, 0, -1):
+remaining_lives = 8
+while remaining_lives > 0:
     print(masked_text)
+    if '-' not in masked_text:
+        break
+
     user_guess = input("Input a letter:")
     if user_guess not in selected_word:
-        print("No such letter in the word")
+        print("That letter doesn't appear in the word")
+        remaining_lives -= 1
+        
+        if remaining_lives != 0:
+            print("")
+        continue
     else:
-        # Remember strings are immutable, so we need to convert the masked word to 
-        # a list of characters so that we can easily (there are other ways sha) replace characters at specific indices
-        # We can't use replace either because all the characters are dashes ('-'). Doing this
-        # will just affect all the dashes
-        char_list = list(masked_text)
-        for index, value in enumerate(masked_text):
-            if user_guess == selected_word[index]:
-                char_list[index] = user_guess
-        masked_text = "".join(char_list)
-    print("")
+        if user_guess in masked_text:
+            print("No improvements")
+            remaining_lives -= 1
 
-print("Thanks for playing!", end='\n')
-print("We'll see how well you did in the next stage")
+            if remaining_lives != 0:
+                print("")
+            continue
+        else:
+            char_list = list(masked_text)
+            for index, value in enumerate(masked_text):
+                if user_guess == selected_word[index]:
+                    char_list[index] = user_guess
+            masked_text = "".join(char_list)
+            print("")
 
+if '-' not in masked_text:
+    print("You guessed the word!")
+    print("You survived!")
 
-
-
-# user_guess = input(f'Guess the word {hint}:')
-# if user_guess == selected_option:
-#     # print(selected_option)
-#     print('You survived!')
-# else:
-#     # print(selected_option)
-#     print('You are hanged!')
+if '-' in masked_text:
+    print("You lost!")
